c3bb7ce0200fce50643de56d9966c52fdc5ccc6f
venkat <v.kannan@infocerv.com>
2022-03-04 12:37:27 +0530

    setup bottom nav with fragments

--- app/src/main/java/com/venkat/sellquick/CartFragment.kt
+++ app/src/main/java/com/venkat/sellquick/CartFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.venkat.sellquick.databinding.FragmentCartBinding
+import com.venkat.sellquick.databinding.FragmentHomeBinding
 
 
 class CartFragment : Fragment() {
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
 }
--- app/src/main/java/com/venkat/sellquick/MainActivity.kt
+++ app/src/main/java/com/venkat/sellquick/MainActivity.kt
@@ -2,10 +2,23 @@ package com.venkat.sellquick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.venkat.sellquick.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav : BottomNavigationView = findViewById(R.id.activity_main_bottom_navigation_view)
+        bottomNav.setupWithNavController(navController)
+
     }
 }
